2ac7f69e59a7639c2c07f1d6dd68fa97eb3d0c14
Marilia-Gomes11022 <160539701+Marilia-Gomes11022@users.noreply.github.com>
2025-09-16 20:29:49 +0000

    homi

--- app.py
+++ app.py
@@ -14,6 +14,9 @@ def contato():
 def exemplo():
     return render_template('exemplo.html')
 
+@app.route('/exemplo2')
+def exemplo2():
+    return render_template('exemplo2.html')
 
 #isto tem que ser a >>ÚLTIMA<< coisa do código
 if __name__ == '__main__':
